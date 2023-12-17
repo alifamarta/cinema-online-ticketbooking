02fbcdfbe75ed488ee9dd4ec1a319b95766c9f52
Alif_Amarta <alifamarta23@gmail.com>
2023-12-17 16:40:13 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,10 +7,7 @@ function signUp() {
 }
 
 function validSignUp() {
-    let firstname = document.getElementById('firstname').value;
-    let lastname = document.getElementById('lastname').value;
     let username = document.getElementById('username').value;
-    let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
 
     if (username.length < 8) {
