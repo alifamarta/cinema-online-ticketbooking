b575d89b1babb4fb149f532364d0d10ebc2a36c8
Alif_Amarta <alifamarta23@gmail.com>
2023-12-17 16:39:34 +0700

    Update

--- src/main.js
+++ src/main.js
@@ -5,3 +5,21 @@ function getBack() {
 function signUp() {
     window.location.href = 'signup.html'
 }
+
+function validSignUp() {
+    let firstname = document.getElementById('firstname').value;
+    let lastname = document.getElementById('lastname').value;
+    let username = document.getElementById('username').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    if (username.length < 8) {
+        alert('Username must be longer than 8 characters');
+        return false
+    } else if (password.length < 8) {
+        alert('Password must be longer than 8 characters');
+        return false
+    } else {
+        return true;
+    }
+}
